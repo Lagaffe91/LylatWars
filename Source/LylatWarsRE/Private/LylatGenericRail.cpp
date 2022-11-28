6379d2be29901ea75b28ea2d05c35a01ce544c99
Alexandre GAUTHIER <al.gauthier@student.isartdigital.com>
2022-11-28 15:57:25 +0100

    Forgot to add assets

--- Source/LylatWarsRE/Private/LylatGenericRail.cpp
+++ Source/LylatWarsRE/Private/LylatGenericRail.cpp
@@ -52,9 +52,9 @@ void ALylatGenericRail::Tick(float DeltaTime)
 				this->RailEnded();
 			}
 		}
-	}
 
-	this->UpdateAllActorsTransform(railTime);
+		this->UpdateAllActorsTransform(railTime);
+	}
 }
 
 void ALylatGenericRail::JoinRail(TSubclassOf<AActor> Actor)
