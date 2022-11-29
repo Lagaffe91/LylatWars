70ef8392d3f533f0c6536be9ac3fdc23ccf7a0fe
Quentin LEPINE <q.lepine@student.isartdigital.com>
2022-11-29 15:34:26 +0100

    edited debug strings

--- Source/LylatWarsRE/Public/DebugString.h
+++ Source/LylatWarsRE/Public/DebugString.h
@@ -1,6 +1,13 @@
 #pragma once
 
+#ifndef ANDROID
 #define Debug(x, ...) if(GEngine){GEngine->AddOnScreenDebugMessage(-1, 15.0f, FColor::Cyan, this->GetName() + L": " + FString::Printf(TEXT(x), __VA_ARGS__));}
 #define DebugError(x, ...) if(GEngine){GEngine->AddOnScreenDebugMessage(-1, 15.0f, FColor::Red, this->GetName() + L": " + FString::Printf(TEXT(x), __VA_ARGS__));}
 #define DebugWarning(x, ...) if(GEngine){GEngine->AddOnScreenDebugMessage(-1, 15.0f, FColor::Yellow, this->GetName() + L": " + FString::Printf(TEXT(x), __VA_ARGS__));}
 #define DebugFVector(x) if(GEngine){GEngine->AddOnScreenDebugMessage(-1, 15.0f, FColor::Cyan, this->GetName() + L": " + FString::Printf(TEXT("%f, %f, %f"), x.X, x.Y, x.Z));}
+#else
+#define Debug(x, ...) if(GEngine){GEngine->AddOnScreenDebugMessage(-1, 15.0f, FColor::Cyan, this->GetName() + ": " + FString::Printf(TEXT(x), __VA_ARGS__));}
+#define DebugError(x, ...) if(GEngine){GEngine->AddOnScreenDebugMessage(-1, 15.0f, FColor::Red, this->GetName() + ": " + FString::Printf(TEXT(x), __VA_ARGS__));}
+#define DebugWarning(x, ...) if(GEngine){GEngine->AddOnScreenDebugMessage(-1, 15.0f, FColor::Yellow, this->GetName() + ": " + FString::Printf(TEXT(x), __VA_ARGS__));}
+#define DebugFVector(x) if(GEngine){GEngine->AddOnScreenDebugMessage(-1, 15.0f, FColor::Cyan, this->GetName() + ": " + FString::Printf(TEXT("%f, %f, %f"), x.X, x.Y, x.Z));}
+#endif
