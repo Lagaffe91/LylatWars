ef670a68d3418e68ba82b03dd7de41546b69e9f9
Alexandre GAUTHIER <al.gauthier@student.isartdigital.com>
2022-11-30 15:32:49 +0100

    New enemy : Falcon (high speed + barrel roll)

---
+++ Source/LylatWarsRE/Public/LylatFalconEnemy.h
@@ -0,0 +1,46 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "LylatEnemy.h"
+#include "LylatFalconEnemy.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class LYLATWARSRE_API ALylatFalconEnemy : public ALylatEnemy
+{
+	GENERATED_BODY()
+public :
+	/**Cooldown between two shots (in seconds)*/
+	UPROPERTY(Category = "Falcon", EditAnywhere, BlueprintReadOnly, meta = (ClampMin = "0"))
+		float FalconRollCooldown = 5;
+	/**Random number between 0 and FalcomRandomCooldown to be added to the original cooldown for more variations*/
+	UPROPERTY(Category = "Falcon", EditAnywhere, BlueprintReadOnly, meta = (ClampMin = "0"))
+		int FalcomRandomCooldown = 5;
+	/**Animation duration (in seconds)*/
+	UPROPERTY(Category = "Falcon", EditAnywhere, BlueprintReadOnly, meta = (ClampMin = "0"))
+		float AnimationDuration = 0.5;
+
+	/**Represent the state of the roll cooldown (in seconds)*/
+	UPROPERTY(Category = "Falcon", VisibleInstanceOnly, BlueprintReadOnly)
+		float FalconRollTimer = 0;
+	/**Represent the state of the animation (in seconds)*/
+	UPROPERTY(Category = "Falcon", VisibleInstanceOnly, BlueprintReadOnly)
+		float AnimationTimer = 0;
+	
+
+public : 
+	void Behaviour_Implementation(float DeltaTime);
+
+	void Animate_Implementation(float DeltaTime);
+
+private :
+	void DoBarrelRoll(float DeltaTime);
+	void ResetRollCooldown();
+
+private :
+	bool isRolling = false; 
+};
