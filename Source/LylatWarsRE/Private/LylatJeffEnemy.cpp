9d085b530eb24cbc8590cc9695f591423768fb67
Quentin LEPINE <q.lepine@student.isartdigital.com>
2022-12-03 23:41:03 +0100

    Added ragdoll to enemy Miku, added new enemy Jeff

--- Source/LylatWarsRE/Private/LylatJeffEnemy.cpp
+++ Source/LylatWarsRE/Private/LylatJeffEnemy.cpp
@@ -1,5 +1,98 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
-
 #include "LylatJeffEnemy.h"
 
+#include "LylatPlayerPawn.h"
+
+#include "DebugString.h"
+
+ALylatJeffEnemy::ALylatJeffEnemy()
+{
+	TurretMesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("TurretMesh"));
+	TurretMesh->SetupAttachment(EntityMesh);
+	CannonMesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("CannonMesh"));
+	CannonMesh->SetupAttachment(TurretMesh);
+	ResetShotTimer();
+}
+
+void ALylatJeffEnemy::BeginPlay()
+{
+	Super::BeginPlay();
+
+	UpdateQuat();
+}
+
+void ALylatJeffEnemy::Behaviour_Implementation(float DeltaTime)
+{
+	if (JeffShouldUpdateQuat)
+	{
+		UpdateQuat();
+	}
+	bool facing = FaceTowardsPlayer();
+	if (JeffShotTimer > 0)
+	{
+		JeffShotTimer -= DeltaTime;
+	}
+	else if (facing)
+	{
+		ResetShotTimer();
+		ShootBullet();
+	}
+}
+
+void ALylatJeffEnemy::Animate_Implementation(float DeltaTime)
+{
+}
+
+void ALylatJeffEnemy::ResetShotTimer()
+{
+	JeffShotTimer = JeffShotCooldown + FMath::RandRange(0.f, JeffRandomCooldown);
+}
+
+void ALylatJeffEnemy::SetCooldown(const float& NewCooldown)
+{
+	if (NewCooldown > 0)
+	{
+		JeffShotCooldown = NewCooldown;
+	}
+}
+
+bool ALylatJeffEnemy::FaceTowardsPlayer()
+{
+	FVector dir = PlayerReference->EntityMesh->GetComponentLocation() - CannonMesh->GetComponentLocation();
+	if (dir.Size() > JeffDetectionMaxRange) return false;
+	dir.Normalize();
+	dir = invRotation * dir;
+	FVector targetRotation = dir.ToOrientationQuat().Euler();
+	if (targetRotation.Z < -JeffHorizontalRange || targetRotation.Z > JeffHorizontalRange ||
+		targetRotation.Y < -JeffVerticalMinRange || targetRotation.Y > JeffVerticalMaxRange) return false;
+	TurretMesh->SetRelativeRotation(FQuat::MakeFromEuler(FVector(0, 0, targetRotation.Z)));
+	CannonMesh->SetRelativeRotation(FQuat::MakeFromEuler(FVector(0,targetRotation.Y,0)));
+	return true;
+}
+
+void ALylatJeffEnemy::ShootBullet()
+{
+	FActorSpawnParameters SpawnParams;
+	SpawnParams.Owner = this;
+	SpawnParams.Instigator = GetInstigator();
+
+	FVector Location = CannonMesh->GetComponentLocation();
+	FRotator Rotation = CannonMesh->GetComponentRotation();
+	ALylatNormalBullet* Projectile = GetWorld()->SpawnActor<ALylatNormalBullet>(BulletType, Location, Rotation, SpawnParams);
+
+	if (Projectile)
+	{
+		//Set the projectile's initial trajectory.
+		FVector LaunchDirection = CannonMesh->GetComponentQuat().GetForwardVector();
+		Projectile->FireInDirection(LaunchDirection, this);
+	}
+}
+
+int ALylatJeffEnemy::GetScoreAmount()
+{
+	return 300;
+}
+
+void ALylatJeffEnemy::UpdateQuat()
+{
+	invRotation = RootComponent->GetComponentQuat().Inverse();
+}
--- Source/LylatWarsRE/Public/LylatJeffEnemy.h
+++ Source/LylatWarsRE/Public/LylatJeffEnemy.h
@@ -14,4 +14,68 @@ class LYLATWARSRE_API ALylatJeffEnemy : public ALylatEnemy
 {
 	GENERATED_BODY()
 	
+public:
+	ALylatJeffEnemy();
+
+	virtual void BeginPlay() override;
+
+	/**Cooldown between two shots (in seconds)*/
+	UPROPERTY(Category = "Jeff", EditAnywhere, BlueprintReadOnly, meta = (ClampMin = "0"))
+		float JeffShotCooldown = 5;
+	/**Random Cooldown added between two shots (in seconds)*/
+	UPROPERTY(Category = "Jeff", EditAnywhere, BlueprintReadOnly, meta = (ClampMin = "0"))
+		float JeffRandomCooldown = 2;
+
+	UPROPERTY(Category = "Jeff", VisibleInstanceOnly, BlueprintReadOnly)
+		float JeffShotTimer = 5;
+
+	/**Horizontal range of the enemy in degrees (a value of 0 will lock the enemy to the front, 180 allows him to rotate completly)*/
+	UPROPERTY(Category = "Jeff", EditAnywhere, BlueprintReadOnly, meta = (ClampMax = "180", ClampMin = "0"))
+		float JeffHorizontalRange = 90;
+
+	/**Vertical lower range of the enemy in degrees*/
+	UPROPERTY(Category = "Jeff", EditAnywhere, BlueprintReadOnly, meta = (ClampMax = "180", ClampMin = "0"))
+		float JeffVerticalMinRange = 10;
+
+	/**Vertical upper range of the enemy in degrees*/
+	UPROPERTY(Category = "Jeff", EditAnywhere, BlueprintReadOnly, meta = (ClampMax = "180", ClampMin = "0"))
+		float JeffVerticalMaxRange = 50;
+
+	/**Max detection range of the enemy, player farther away will be ignored*/
+	UPROPERTY(Category = "Jeff", EditAnywhere, BlueprintReadOnly, meta = (ClampMin = "0"))
+		float JeffDetectionMaxRange = 5000;
+
+	/**Should the enemy recalculate its position each frame (activate this if the enemy is on a rail)*/
+	UPROPERTY(Category = "Jeff", EditAnywhere, BlueprintReadOnly)
+		bool JeffShouldUpdateQuat = false;
+
+	/**Mesh component for the turret*/
+	UPROPERTY(Category = "Jeff", VisibleAnywhere, BlueprintReadOnly)
+		class UStaticMeshComponent* TurretMesh;
+
+	/**Mesh component for the cannon*/
+	UPROPERTY(Category = "Jeff", VisibleAnywhere, BlueprintReadOnly)
+		class UStaticMeshComponent* CannonMesh;
+
+	void Behaviour_Implementation(float DeltaTime) override;
+
+	void Animate_Implementation(float DeltaTime) override;
+
+	UFUNCTION(BlueprintCallable, Category = "Jeff")
+		void ResetShotTimer();
+
+	UFUNCTION(BlueprintCallable, Category = "Jeff")
+		void SetCooldown(const float& NewCooldown);
+
+	/**Rotates the turret towards the player, will return false if the action fails (outside of rotation range, or too far away)*/
+	UFUNCTION(BlueprintCallable, Category = "Jeff")
+		bool FaceTowardsPlayer();
+
+	virtual void ShootBullet() override;
+
+	virtual int GetScoreAmount() override;
+
+	void UpdateQuat();
+protected:
+	FQuat invRotation;
 };
