c266ff69a742c25ca52d4ec8c7c58ad1386431a5
Quentin LEPINE <q.lepine@student.isartdigital.com>
2022-12-07 11:53:52 +0100

    build lights, added maps to game data

--- Source/LylatWarsRE/Public/DebugString.h
+++ Source/LylatWarsRE/Public/DebugString.h
@@ -6,8 +6,8 @@
 #define DebugWarning(x, ...) if(GEngine){GEngine->AddOnScreenDebugMessage(-1, 15.0f, FColor::Yellow, this->GetName() + L": " + FString::Printf(TEXT(x), __VA_ARGS__));}
 #define DebugFVector(x) if(GEngine){GEngine->AddOnScreenDebugMessage(-1, 15.0f, FColor::Cyan, this->GetName() + L": " + FString::Printf(TEXT("%f, %f, %f"), x.X, x.Y, x.Z));}
 #else
-#define Debug(x, ...) if(GEngine){GEngine->AddOnScreenDebugMessage(-1, 15.0f, FColor::Cyan, this->GetName() + ": " + FString::Printf(TEXT(x), __VA_ARGS__));}
-#define DebugError(x, ...) if(GEngine){GEngine->AddOnScreenDebugMessage(-1, 15.0f, FColor::Red, this->GetName() + ": " + FString::Printf(TEXT(x), __VA_ARGS__));}
-#define DebugWarning(x, ...) if(GEngine){GEngine->AddOnScreenDebugMessage(-1, 15.0f, FColor::Yellow, this->GetName() + ": " + FString::Printf(TEXT(x), __VA_ARGS__));}
-#define DebugFVector(x) if(GEngine){GEngine->AddOnScreenDebugMessage(-1, 15.0f, FColor::Cyan, this->GetName() + ": " + FString::Printf(TEXT("%f, %f, %f"), x.X, x.Y, x.Z));}
+#define Debug(x, ...) 
+#define DebugError(x, ...) 
+#define DebugWarning(x, ...) 
+#define DebugFVector(x) 
 #endif
